77dee846073b6c9b3352e448cd1d28a3999e8310
Whee-il <willcockrum5@gmail.com>
2019-01-25 09:47:58 -0800

    Minor style changes.  Just making sure Git is up to date

--- src/main/java/org/usfirst/frc/team1160/robot/subsystems/DriveTrain.java
+++ src/main/java/org/usfirst/frc/team1160/robot/subsystems/DriveTrain.java
@@ -212,7 +212,6 @@ public class DriveTrain extends Subsystem implements RobotMap {
 		System.out.println(gyro.getYaw());
 		gyro.reset();
 		gyro.zeroYaw();
-		
 		System.out.println(gyro.getYaw());
 	}
 	public void zeroGyro() {
